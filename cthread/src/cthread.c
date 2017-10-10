3bb29fee8c2ef97c9ff1aec750105d13bf5b56f9
APNeto <penaneto@gmail.com>
2017-10-10 12:09:07 -0300

    Criando main e outras threads em apto

--- cthread/src/cthread.c
+++ cthread/src/cthread.c
@@ -1,11 +1,13 @@
 #define ERRO -1
+#define SUCESSO 1
 
 PFILA2 aptos;
 PFILA2 bloqueados;
 PFILA2 executando;
+int numT = 0;
 
 int cidentify (char *name, int size){
-  return ERRO;
+  return ERRO;T
 }
 
 
@@ -15,17 +17,36 @@ int iniciaThreads(){
   CreateFila2(executando);
   
   TCB_t* tcbmain = (TCB_t*) malloc(sizeof(TCB_t));
-  tcbmain.tid = 0;
-  tcbmaind.state = PROCST_EXEC;
-  //tcbmain.context
+  tcbmain->tid = numT++;
+  tcbmaind->state = PROCST_EXEC;
+  tcbmain->context.uc_link = NULL;
+  tcbmain->context.uc_stack.ss_sp = malloc(SIGSTKSZ);
+  tcbmain->context.uc_stack.size = SIGSTKSZ;
+
+  AppendFila2(executando, tcbmain);
+  getcontext(&(tcbmain->context));
   
-  return ERRO;
+  return SUCESSO;
 }
 
 int ccreate (void* (*start)(void*), void *arg, int prio) {
   if(!FirstFila2(executando)) { //caso ainda nao tenha sido criada fila de executando, criar filas e iniciar thread main
     iniciaThreads();
-  } 
+  }
+
+  // Cadastra nova thread de start
+  TCB_t* tcb = (TCB_t*) malloc(sizeof(TCB_t));
+  tcb->tid = numT++;
+  tcb->state = PROCST_EXEC;
+  tcb->context.uc_link = NULL;
+  tcb->context.uc_stack.ss_sp = malloc(SIGSTKSZ);
+  tcb->context.uc_stack.size = SIGSTKSZ;
+  getcontext(&(tcb->context));
+
+  AppendFila2(aptos, tcb);
+
+  return tcb->tid;
+
   return ERRO;
 }
 
